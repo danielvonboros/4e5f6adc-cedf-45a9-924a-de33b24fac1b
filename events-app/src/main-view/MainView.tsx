75f3cec08dc8dbfd0566381d17ae1ccbdd941d99
Daniel von Boros <dvb@maltego.com>
2022-07-05 09:13:54 +0200

    feat: axios request to fetch data

--- events-app/src/main-view/MainView.tsx
+++ events-app/src/main-view/MainView.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import axios from "axios";
 
 const MainView: React.FC = () => {
+  axios
+    .get("https://tlv-events-app.herokuapp.com/events/uk/london")
+    .then((data) => console.log(data));
   return <div>MainView</div>;
 };
 
